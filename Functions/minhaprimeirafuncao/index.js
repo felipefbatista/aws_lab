a0ed21f88004394b98ecebe8b8ab478976775bff
Felipe De Freitas Batista <51919144+felipefbatista@users.noreply.github.com>
2020-08-28 11:37:17 -0300

    Update index.js

--- Functions/minhaprimeirafuncao/index.js
+++ Functions/minhaprimeirafuncao/index.js
@@ -5,6 +5,9 @@ exports.handler = async (event) => {
         body: JSON.stringify('Hello from Lambda!'),
     };
     console.log("eu sou um log do cloud watch");
+    console.error("eu sou um error do cloud watch");
+    console.info("eu sou um info do cloud watch");
+
     console.log({type:"info", body:{abc:1, efg:2}});
     console.log({type:"warning", body:{abc:3, efg:4}});
 
